e57816b24f5c55f3f11f177db7a2f79d41984591
Maluxuxu <larlu1466@gmail.com>
2018-07-16 18:21:57 -0300

    db

---
+++ controledegastos/src/main/java/cliente.java
@@ -0,0 +1,87 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author aluno
+ */
+public class cliente {
+
+    /**
+     * @return the codidentificacao
+     */
+    public String getCodidentificacao() {
+        return codidentificacao;
+    }
+
+    /**
+     * @param codidentificacao the codidentificacao to set
+     */
+    public void setCodidentificacao(String codidentificacao) {
+        this.codidentificacao = codidentificacao;
+    }
+
+    /**
+     * @return the email
+     */
+    public String getEmail() {
+        return email;
+    }
+
+    /**
+     * @param email the email to set
+     */
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    /**
+     * @return the nomeFantasia
+     */
+    public String getNomeFantasia() {
+        return nomeFantasia;
+    }
+
+    /**
+     * @param nomeFantasia the nomeFantasia to set
+     */
+    public void setNomeFantasia(String nomeFantasia) {
+        this.nomeFantasia = nomeFantasia;
+    }
+
+    /**
+     * @return the razaoSocial
+     */
+    public String getRazaoSocial() {
+        return razaoSocial;
+    }
+
+    /**
+     * @param razaoSocial the razaoSocial to set
+     */
+    public void setRazaoSocial(String razaoSocial) {
+        this.razaoSocial = razaoSocial;
+    }
+
+    /**
+     * @return the tipoCliente
+     */
+    public String getTipoCliente() {
+        return tipoCliente;
+    }
+
+    /**
+     * @param tipoCliente the tipoCliente to set
+     */
+    public void setTipoCliente(String tipoCliente) {
+        this.tipoCliente = tipoCliente;
+    }
+   private String codidentificacao;
+   private String email;
+   private String nomeFantasia;
+   private String razaoSocial;
+   private String tipoCliente;
+}
---
+++ controledegastos/src/main/java/ordemdeservico.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author aluno
+ */
+public class ordemdeservico {
+
+    /**
+     * @return the quantidade
+     */
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    /**
+     * @param quantidade the quantidade to set
+     */
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    /**
+     * @return the valor
+     */
+    public double getValor() {
+        return valor;
+    }
+
+    /**
+     * @param valor the valor to set
+     */
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+   private int quantidade;
+   private double valor;
+}
